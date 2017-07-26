1ca246e053823ae88690c9c6aa7b7d45eea59f09
a.gubankov <al_1001@ukr.net>
2017-07-26 18:43:36 +0300

    нихрена не получается!

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,43 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+          if (storage[i]==null)
+              storage[i]=r;
+          break;
+            }
+        }
+
+
+    String get(String uuid) {
+        return uuid;
     }
 
-    Resume get(String uuid) {
-        return null;
-    }
+
 
     void delete(String uuid) {
+        remove ;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        for (int i = 0; i < storage.length; i++) {
+          if (storage[i] != null) return new Resume[]{storage[i]};
+        }
+
+
+        return null;
     }
 
     int size() {
-        return 0;
+        return storage.length;
     }
 }
