4f7e409508c05d7c627811f2a2abe13a3aacb01c
a.gubankov <>
2019-06-26 18:01:16 +0300

    Remove toString in metod Get

--- src/main/java/ArrayStorage.java
+++ src/main/java/ArrayStorage.java
@@ -58,7 +58,7 @@ public class ArrayStorage {
     Resume get(String uuid) {
         isResumePresent(uuid);
         for (int i = 0; i < size; i++) {
-            if (uuid.toString().equals(storage[i])) {
+            if (uuid.equals(storage[i])) {
                 printExists();
                 return storage[i];
 
